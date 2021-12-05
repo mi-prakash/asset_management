1d8f0f2ec5e12ee0fb84b59a631de7b194e2be8d
prakash-a7x <prakash.a7x@gmail.com>
2021-12-05 20:46:16 +0600

    first commit

---
+++ database/migrations/2021_12_05_124719_create_assets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('category');
+            $table->integer('assigned_to')->default(0);
+            $table->timestamp('assigned_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assets');
+    }
+}
